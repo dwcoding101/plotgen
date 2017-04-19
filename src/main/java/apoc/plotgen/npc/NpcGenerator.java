62a52649fc09f525f92707a69f9758830c8a5de4
Daniel <dwrunner26@gmail.com>
2017-04-19 17:38:31 +0100

    plotGen NpcGenerator

---
+++ src/main/java/apoc/plotgen/npc/NpcGenerator.java
@@ -0,0 +1,55 @@
+package apoc.plotgen.npc;
+
+import apoc.plotgen.gender.GenderGenerator;
+import apoc.plotgen.names.NameGenerator;
+import apoc.result.GraphResult;
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.Result;
+import org.neo4j.procedure.Context;
+import org.neo4j.procedure.Description;
+import org.neo4j.procedure.Procedure;
+import org.neo4j.procedure.UserFunction;
+
+import java.util.UUID;
+import java.util.stream.Stream;
+
+/**
+ * Created by Daniel on 19/04/2017.
+ */
+public class NpcGenerator {
+
+    @Context
+    public GraphDatabaseService db;
+
+    @Procedure(name = "apoc.plotgen.npc.CreateNPC")
+    @Description( "apoc.plotgen.npc.CreateNPC - create a new NPC" )
+    public void CreateNPC() {
+
+        String uuid = uuid();
+        String gender = new GenderGenerator().gender();
+        String firstName = "";
+        if(gender.equals("Female")){
+            firstName = new NameGenerator().humanFemale();
+        }
+        else {
+            firstName = new NameGenerator().humanMale();
+        }
+        String surname = new NameGenerator().humanSurnameP1()+new NameGenerator().humanSurnameP2();
+
+        String query = "CALL apoc.create.node(['NPC'], {uuid:"+ uuid +
+                                                     ", firstName:'" + firstName +"'"+
+                                                     ", surname:" + surname +"'"+
+                                                     ", gender:" + gender +"'"+
+                                                     "})";
+
+        Result execute = db.execute(query);
+    }
+
+
+
+    @UserFunction
+    @Description("apoc.create.uuid() - creates an UUID")
+    public String uuid() {
+        return UUID.randomUUID().toString();
+    }
+}
