88e4364d027620b9e0b375ac8703f145d15d827a
Daniel <dwrunner26@gmail.com>
2017-04-20 11:36:56 +0100

    plotGen NpcGenerator

--- src/main/java/apoc/plotgen/time/Time.java
+++ src/main/java/apoc/plotgen/time/Time.java
@@ -19,7 +19,7 @@ public class Time {
     @Context
     public Log log;
 
-    @Procedure(name = "apoc.plotgen.npc.CreateTime", mode = Mode.WRITE)
+    @Procedure(name = "apoc.plotgen.time.CreateTime", mode = Mode.WRITE)
     @Description( "apoc.plotgen.npc.CreateNPC - create a new NPC" )
     public void CreateTime() {
 
@@ -33,8 +33,8 @@ public class Time {
         Result execute = db.execute(query);
     }
 
-    @Procedure(name = "apoc.plotgen.npc.SetDayLength", mode = Mode.WRITE)
-    @Description( "apoc.plotgen.npc.SetDayLength - sets the length of the world day in seconds" )
+    @Procedure(name = "apoc.plotgen.time.SetDayLength", mode = Mode.WRITE)
+    @Description( "apoc.plotgen.time.SetDayLength - sets the length of the world day in seconds" )
     public void SetDayLength(@Name("Second") long second) {
 
         String query = "MATCH (a:TIME)) WITH a SET a.dayLength = '"+ second +"'";
@@ -42,8 +42,8 @@ public class Time {
         Result execute = db.execute(query);
     }
 
-    @Procedure(name = "apoc.plotgen.npc.SetDayInYear", mode = Mode.WRITE)
-    @Description( "apoc.plotgen.npc.SetDayInYear - sets the number of the world days in the worlds")
+    @Procedure(name = "apoc.plotgen.time.SetDayInYear", mode = Mode.WRITE)
+    @Description( "apoc.plotgen.time.SetDayInYear - sets the number of the world days in the worlds")
     public void SetDayInYear(@Name("dayInYear") long days) {
 
         String query = "MATCH (a:TIME)) WITH a SET a.daysInYear = '"+ days +"'";
@@ -51,8 +51,8 @@ public class Time {
         Result execute = db.execute(query);
     }
 
-    @Procedure(name = "apoc.plotgen.npc.AdvanceADay", mode = Mode.WRITE)
-    @Description( "apoc.plotgen.npc.AdvanceADay - Advance Time one day")
+    @Procedure(name = "apoc.plotgen.time.AdvanceADay", mode = Mode.WRITE)
+    @Description( "apoc.plotgen.time.AdvanceADay - Advance Time one day")
     public void AdvanceADay() {
 
         // Get day length in seconds
